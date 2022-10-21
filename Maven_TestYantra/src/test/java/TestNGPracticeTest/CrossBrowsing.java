e9fa16559ac58e907360e1490c13cdbfd22adc2e
saitharun996 <saitharun996@gmail.com>
2022-10-21 09:53:21 +0530

    Start

---
+++ Maven_TestYantra/src/test/java/TestNGPracticeTest/CrossBrowsing.java
@@ -0,0 +1,81 @@
+package TestNGPracticeTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.genericutility.PropertyFileUtility;
+import com.genericutility.WebDriverUtility;
+
+import Object_Repositories.HomePageClass;
+import Object_Repositories.LoginPageClass;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CrossBrowsing 
+{
+	public WebDriver driver;
+	public static WebDriver sdriver;
+	PropertyFileUtility propertyFileUtility=new PropertyFileUtility();
+	
+	@BeforeSuite(groups = {"smoke","regression","integration"})
+	public void dataBaseConnection()
+	{
+		System.out.println("===Connected to Database===");
+	}
+	
+	
+	@AfterSuite(groups = {"smoke","regression","integration"})
+	public void disConnectDatavbase() {
+		System.out.println("===Dis Connected===");
+	}
+	
+
+	/*
+	 * Launch the browser
+	 */
+	
+	@BeforeClass(groups = {"smoke","regression","integration"})
+	public void launchBrowser() throws Throwable 
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		sdriver=driver;
+		WebDriverUtility webDriverUtility=new WebDriverUtility(driver);
+		webDriverUtility.maximize();
+		driver.get(propertyFileUtility.readDataFromProprtyFile("url"));
+		webDriverUtility.implicitlywait();
+
+}
+
+
+
+	@AfterClass(groups = {"smoke","regression","integration"})
+	public void closeBrowser() {
+		driver.close();
+	}
+
+	@BeforeMethod (groups = {"smoke","regression","integration"})
+	public void logintoApp() throws Throwable {
+
+
+		String uName = propertyFileUtility.readDataFromProprtyFile("username");
+		String pWD = propertyFileUtility.readDataFromProprtyFile("password");
+
+		LoginPageClass lpc=new LoginPageClass(driver);
+		lpc.login(uName, pWD);
+	}
+	@AfterMethod(groups = {"smoke","regression","integration"})
+	public void logoutfromApp() 
+	{
+		HomePageClass homePageClass = new HomePageClass(driver);
+		homePageClass.signout(driver);
+
+	}
+
+
+}
