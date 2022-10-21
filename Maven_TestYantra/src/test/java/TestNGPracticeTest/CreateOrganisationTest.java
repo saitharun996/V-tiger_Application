e202870b1d7a83ddd81b09e8779eb5290bcb02ea
saitharun996 <saitharun996@gmail.com>
2022-10-21 10:55:55 +0530

    changes made in pom.xml

--- Maven_TestYantra/src/test/java/TestNGPracticeTest/CreateOrganisationTest.java
+++ Maven_TestYantra/src/test/java/TestNGPracticeTest/CreateOrganisationTest.java
@@ -2,22 +2,16 @@ package TestNGPracticeTest;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
-import com.genericutility.BaseClass;
 import com.genericutility.BaseClassTest;
 import com.genericutility.JavaUtility;
 import com.genericutility.ListnersImplementation;
 
 import Object_Repositories.NewOrgPageClass;
 import Object_Repositories.OrgPageClass;
-//@Listeners(ListnersImplementation.class)
+@Listeners(ListnersImplementation.class)
 public class CreateOrganisationTest extends BaseClassTest
 {
-	//extends properties from baseclasstest
 	@Test(groups = "regression")
 	public void CreateOrg()
 	{
